1b9a4f2cb4fe702357329eec6b51e9ab126549f1
Kristoffer <kristoffer.sorquist@nilsongroup.com>
2019-03-01 10:21:29 +0100

    Create SleepingHours.js

---
+++ SleepingHours.js
@@ -0,0 +1,35 @@
+// The following script returns your sleeping hours based on your motions and alarm sensors
+// Returns when you went to bed - when you got up. (e.g. 23:20:00 - 08:10:00)
+
+Date.prototype.readableTime = function() {
+    return `${("0"+this.getHours()).slice(-2)}:${("0"+this.getMinutes()).slice(-2)}:${("0"+this.getSeconds()).slice(-2)}`;
+};
+
+async function logs() {
+    let capability = [ "alarm_motion", "alarm_contact"];
+    let devices = await Homey.devices.getDevices();
+    let alerts = [];
+    for (var d in devices) {
+        let device = devices[d];
+        let capObj = _.find(device.capabilitiesObj, function(o) { return capability.includes(o.id); });
+        if (!capObj) continue;
+        let i = _.find(device.insights, function(o) { return o.id === capObj.id; });
+        let insights = await Homey.insights.getLogEntries({id: i.id, uri: i.uri});    
+        alerts = _.concat(alerts, _.map(_.filter(insights.values, function (o) { return o.v; }), function(o) { return new Date(o.t); }));
+    }
+    return _.sortBy(alerts, function(value) {return value;});
+}
+
+let startSleeping = new Date();
+startSleeping.setHours(2);
+startSleeping.setMinutes(0);
+
+let stopSleeping = new Date();
+stopSleeping.setHours(5);
+stopSleeping.setMinutes(0);
+
+var logs = await logs();
+var s = _.last(_.filter(logs, function(o) { return o < startSleeping; })).readableTime();
+var e = _.filter(logs, function(o) { return o > stopSleeping; })[0].readableTime();
+
+return `${s} - ${e}`;
