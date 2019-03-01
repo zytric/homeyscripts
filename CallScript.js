5a5bb69e603d5661e8167eac29e9465ba87e5833
Kristoffer <kristoffer.sorquist@nilsongroup.com>
2019-03-01 15:03:45 +0100

    Create CallScript.js

---
+++ CallScript.js
@@ -0,0 +1,15 @@
+// This is a sample of how to call another script file.
+
+// If your file is "Cellsynt" (in homeyscript.homey.app)
+
+// The parameters is used in the script (Cellsynt in this case):
+// args[0] = to 
+// args[1] = message 
+
+async function sms(to, message) {
+    let hs = await Homey.apps.getApp({ id: "com.athom.homeyscript" });
+    var r = await hs.apiPost("script/Cellsynt/Run", [to, message]); //Input parameters
+    console.log(r.returns);
+}
+
+await sms("0700000000", "Hello 🎂!"");
