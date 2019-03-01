1f9575d32434824fa28c7418749945b8b7b8da89
Kristoffer <kristoffer.sorquist@nilsongroup.com>
2019-03-01 14:39:25 +0100

    Update SmsCellsynt.js

--- SmsCellsynt.js
+++ SmsCellsynt.js
@@ -1,14 +1,18 @@
 // Send SMS for 0,5kr/sms
 // https://www.cellsynt.com/pdf/Cellsynt_SMS_gateway_HTTP_interface_(Swedish).pdf
 
-async function sms(destination, text, originator = "46700000000") {
+async function sms(destination, text, originator = "46700000123") {
     let username = "username";
     let password = "password";
     let url = `https://se-1.cellsynt.net/sms.php?username=${username}&password=${password}&destination=${destination}&type=text&charset=UTF8&originatortype=alpha&originator=${originator}&text=${encodeURIComponent(text)}`;
     return fetch(url).then(function(response) {
         return response.text()
     }).then(function(html) {
-        return html;
+        console.log(html);
+        let trackingId = html.indexOf("OK: ") !== -1 ? html.slice(4).trim() : null;
+        if (trackingId)
+            return trackingId;
+        return null;
     });
 }
 
